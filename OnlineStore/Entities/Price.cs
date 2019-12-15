d05620b3189623c962688eaaf0bfd0acc197bb7a
den090312 <31979965+den090312@users.noreply.github.com>
2019-12-15 14:10:27 +0400

    price fix

--- OnlineStore/Entities/Price.cs
+++ OnlineStore/Entities/Price.cs
@@ -6,10 +6,10 @@ namespace Entities
 {
     public class Price
     {
+        public DateTime Date { get; set; }
+
         public Currency Currency { get; set; }
 
         public decimal Value { get; set; }
-
-        public DateTime Date { get; set; }
     }
 }
