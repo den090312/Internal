86e3a83e3d865358e524296d5679291934103e68
Denis <den090312@gmail.com>
2019-12-22 23:44:37 +0300

    ToDo Validator

--- OnlineStore/Interfaces/IValidateLogic.cs
+++ OnlineStore/Interfaces/IValidateLogic.cs
@@ -7,5 +7,7 @@ namespace InterfacesBLL
     public interface IValidateLogic
     {
         bool IsValid<T>(T validatedObject, out List<KeyValuePair<string, string>> errors) where T: class;
+
+        //ToDo сделать сущность Validator вместо кортежа
     }
 }
