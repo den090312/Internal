4d4a851c17cf912519af9f6e0374f18a384ea9da
Denis <den090312@gmail.com>
2019-12-29 10:37:24 +0300

    Val Obj refactor

--- OnlineStore/DTO/ValidatableObject.cs
+++ OnlineStore/DTO/ValidatableObject.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 
 namespace DTO
 {
-    public class ValidatableObject<O>
+    public class ValidatableObject<O, F>
     {
         public bool IsValid { get; set; }
 
         public O Obj { get; }
 
-        public List<(string fieldName, List<Error> Errors)> ListError { get; set; }
+        public List<ValidatableField<F>> Fields { get; set; }
 
         public ValidatableObject(O obj)
         {
             Obj = obj;
             IsValid = true;
-            ListError = new List<(string fieldName, List<Error> Errors)>();
+            Fields = new List<ValidatableField<F>>();
         }
     }
 }
